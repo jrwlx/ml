c2c298592d03ebfa267648918f4d6555e7787f57
Jarren <josejarren@gmail.com>
2023-01-30 20:55:19 -0800

    changed training model and added visual

--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -24,6 +24,28 @@ y = np.array(data[predict]) # array that contains label
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
+"""
+# able to remove this because we are loading an existing model from pickle file
+# training model with the best possible accuracy
+best = 0
+for _ in range(20):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print("Accuracy: " + str(acc))
+
+    # ff the current model has a better score than one we've already trained then save it
+    if acc > best:
+        best = acc
+        with open("studentgrades.pickle", "wb") as f:
+            pickle.dump(linear, f)
+"""
+
+"""
+# able to remove this because we are loading an existing model from pickle file
 # implementing linear regression
 # defining model
 linear = linear_model.LinearRegression()
@@ -36,11 +58,23 @@ accuracy = linear.score(x_test, y_test)
 # saving model with pickle
 with open("studentgrades.pickle", "wb") as f:
     pickle.dump(linear, f)
+"""
+
+# loading model
+pickle_in = open("studentgrades.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 # viewing constants
-# print('Coefficient: \n', linear.coef_) # slope values
-# print('Intercept: \n', linear.intercept_) # intercept
+print('Coefficient: \n', linear.coef_) # slope values
+print('Intercept: \n', linear.intercept_) # intercept
 
 predictions = linear.predict(x_test) # gets a list of all predictions
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+plot = "G1"
+plt.scatter(data[plot], data["G3"])
+plt.legend(loc=4)
+plt.xlabel(plot)
+plt.ylabel("Final Grade")
+plt.show()
